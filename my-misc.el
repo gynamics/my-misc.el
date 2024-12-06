2209166465321a6eca363cc4155bd08797871672
gynamics <dybfysiat@163.com>
2024-12-06 20:23:40 +0800

    Update code of several functions.

--- my-misc.el
+++ my-misc.el
@@ -60,36 +60,60 @@
                            ".assets")))
     (unless (file-exists-p asset-dir)
       (mkdir asset-dir))
-    (concat (file-name-as-directory asset-dir) output-file)
-    ))
+    (concat (file-name-as-directory asset-dir)
+            output-file)))
 
-(defun my:make-autoloads-subdirectories (dir)
-  "Make autoloads for all subdirectories under DIR."
+(defun my:loaddefs-regenerate (dir &optional generate-full)
+  "Regenerate loaddefs for given DIR.
+Universal argument GENERATE-FULL is passed to `loaddefs-generate'."
+  (interactive "DPath of pacakge: \nP")
+  (let ((output
+         (concat (file-name-as-directory dir)
+                 (replace-regexp-in-string
+                  "\\(\\.el\\|-[0-9]\\{1,8\\}\\(\\.[0-9]+\\)\\)+$" ""
+                  (file-name-nondirectory (directory-file-name dir)))
+                 "-autoloads.el")))
+    (loaddefs-generate dir output nil nil nil generate-full)))
+
+(defun my:loaddefs-regenerate-subdirs (dir &optional all-in-one)
+  "Regenerate loaddefs for all subdirectories under DIR.
+When universal argument ALL-IN-ONE is given, compose them into
+one single `loaddefs.el' and byte-compile it."
+  (interactive "DPath of parent directory: \nP")
+  (dolist (child (file-name-all-completions "" dir))
+    (when (and (directory-name-p child)
+               (not (member child '("./" "../" ".git/" "archives/" "gnupg/"))))
+      (message "Regenerate loaddefs for %s ..." child)
+      (let ((subdir (concat (file-name-as-directory dir) child)))
+        (when all-in-one
+          (let ((all-in-file
+                 (concat (file-name-as-directory dir) "loaddefs.el")))
+            (when (file-exists-p all-in-file)
+              (delete-file all-in-file))
+            (loaddefs-generate subdir all-in-file)
+            (byte-compile-file all-in-file))
+          (my:loaddefs-regenerate subdir t))))))
+
+;; there is an async version provided by koishimacs, which needs async
+(defun my:byte-force-recompile-subdirs (dir)
+  "Call `byte-force-recompile' on all subdirectories of DIR."
   (interactive "DPath of parent directory: ")
-  (dolist (file (file-name-all-completions "" dir))
-    (when (and (directory-name-p file)
-               (not (member file '("./" "../" "archives/" "gnupg/"))))
-      (let ((output-file
-             (concat dir file
-                     (replace-regexp-in-string
-                      "-[0-9]\\{1,8\\}\\(\\.[0-9]+\\)+/" "" file)
-                     "-autoloads.el"))
-            )
-        (loaddefs-generate (concat dir file) output-file)
-        (byte-compile-file output-file)))))
+  (dolist (child (file-name-all-completions "" dir))
+    (when (and (directory-name-p child)
+               (not (member child '("./" "../" ".git/" "archives/" "gnupg/"))))
+      (byte-force-recompile
+       (concat (file-name-as-directory dir) child)))))
 
 (defun my:adjust-alpha (fg bg)
-  "Set window opacity to (FG . BG) by setting frame parameter alpha."
+  "Set window opacity to (FG . BG) by setting frame parameter `alpha'."
+  (declare (obsolete my:adjust-alpha-background "29"))
   (interactive
    "nForeground Transparency Value 0 - 100 opaque: \n\
 nBackground Transparency Value 0 - 100 opaque: ")
   (set-frame-parameter (selected-frame) 'alpha `(,fg . ,bg)))
 
-(make-obsolete #'my:adjust-alpha "my:adjust-alpha-background,"
-               (> emacs-major-version 28))
-
 (defun my:adjust-alpha-background (value)
-  "Set window opacity to VALUE by setting frame parameter alpha-background."
+  "Set window opacity to VALUE by setting frame parameter `alpha-background' ."
   (interactive "nTransparency Value 0 - 100 opaque: ")
   (set-frame-parameter (selected-frame) 'alpha-background value))
 
